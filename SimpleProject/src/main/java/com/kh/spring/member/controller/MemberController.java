3a038a160f0619ec085fbe89434e461d0fee4f3d
Rostreaca <freestalere@naver.com>
2025-10-17 17:49:15 +0900

    1017

--- SimpleProject/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ SimpleProject/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -1,5 +1,20 @@
 package com.kh.spring.member.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.spring.member.model.vo.Member;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping("login")
+	public void login(Member member) {
+		// 1. 값뽑기
+		// 2. 데이터가공
+		System.out.println(member);
+		
+		
+	}
+	
 }
